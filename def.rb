fb2fb21a0d076ee37bcbab4247affed52d1aae40
Allen Palacios <apalacios1994@gmail.com>
2021-02-17 01:29:35 +0000

    Automatically backed up by Learn

---
+++ def.rb
@@ -0,0 +1,9 @@
+def square_array(numbers)
+  new_array = []
+  counter = 0 
+  while counter < numbers.length()
+  new_array << (numbers[counter] * numbers[counter])
+  counter += 1
+  end 
+  return new_array
+end
